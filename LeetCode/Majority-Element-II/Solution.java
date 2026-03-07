195f5f165d282665aefc5d21e7c9886239e777f5
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2026-03-07 22:18:20 +0530

    Add solution for Majority Element II

--- LeetCode/Majority-Element-II/Solution.java
+++ LeetCode/Majority-Element-II/Solution.java
@@ -1,29 +1,46 @@
 1class Solution {
 2    public List<Integer> majorityElement(int[] nums) {
-3
-4        int n = nums.length; //size of the array
-5        List<Integer> ls = new ArrayList<>(); // list of answers
-6
-7        //declaring a map:
-8        HashMap<Integer, Integer> mpp = new HashMap<>();
-9
-10        // least occurrence of the majority element:
-11        int mini = (int)(n / 3) + 1;
-12
-13        //storing the elements with its occurnce:
-14        for (int i = 0; i < n; i++) {
-15            int value = mpp.getOrDefault(nums[i], 0);
-16            mpp.put(nums[i], value + 1);
-17
-18            //checking if nums[i] is
-19            // the majority element:
-20            if (mpp.get(nums[i]) == mini) {
-21                ls.add(nums[i]);
-22            }
-23            if (ls.size() == 2) break;
-24        }
+3         int n = nums.length; //size of the array
+4
+5        int cnt1 = 0, cnt2 = 0; // counts
+6        int el1 = Integer.MIN_VALUE; // element 1
+7        int el2 = Integer.MIN_VALUE; // element 2
+8
+9        // applying the Extended Boyer Moore's Voting Algorithm:
+10        for (int i = 0; i < n; i++) {
+11            if (cnt1 == 0 && el2 != nums[i]) {
+12                cnt1 = 1;
+13                el1 = nums[i];
+14            } else if (cnt2 == 0 && el1 != nums[i]) {
+15                cnt2 = 1;
+16                el2 = nums[i];
+17            } else if (nums[i] == el1) cnt1++;
+18            else if (nums[i] == el2) cnt2++;
+19            else {
+20                cnt1--; cnt2--;
+21            }
+22        }
+23
+24        List<Integer> ls = new ArrayList<>(); // list of answers
 25
-26        return ls;
-27    }
-28}
-29
+26        // Manually check if the stored elements in
+27        // el1 and el2 are the majority elements:
+28        cnt1 = 0; cnt2 = 0;
+29        for (int i = 0; i < n; i++) {
+30            if (nums[i] == el1) cnt1++;
+31            if (nums[i] == el2) cnt2++;
+32        }
+33
+34        int mini = (int)(n / 3) + 1;
+35        if (cnt1 >= mini) ls.add(el1);
+36        if (cnt2 >= mini) ls.add(el2);
+37
+38        // Uncomment the following line
+39        // if it is told to sort the answer array:
+40        //Collections.sort(ls); //TC --> O(2*log2) ~ O(1);
+41
+42        return ls;
+43    
+44        
+45    }
+46}
