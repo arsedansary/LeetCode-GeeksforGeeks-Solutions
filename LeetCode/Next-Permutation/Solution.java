a1289e5fba2f5e6a7845ecd8b414e2c659f37402
ARSED ANSARY <90137553+arsedansary@users.noreply.github.com>
2026-05-26 06:52:22 +0530

    Add solution for Next Permutation

--- LeetCode/Next-Permutation/Solution.java
+++ LeetCode/Next-Permutation/Solution.java
@@ -1,35 +1,45 @@
-1import java.util.*;
-2class Solution {
-3    public void nextPermutation(int[] nums) {
-4        int n = nums.length;
-5        int i = n - 2;
-6        // Step 1: find first decreasing element from right
-7        while (i >= 0 && nums[i] >= nums[i + 1]) {
-8            i--;
-9        }
-10        // Step 2: if not found, reverse whole array
-11        if (i < 0) {
-12            reverse(nums, 0, n - 1);
-13            return;
-14        }
-15        // Step 3: find element just larger than nums[i]
-16        int j = n - 1;
-17        while (nums[j] <= nums[i]) {
-18            j--;
-19        }
-20        // Step 4: swap
-21        swap(nums, i, j);
-22        // Step 5: reverse remaining suffix
-23        reverse(nums, i + 1, n - 1);
-24    }
-25    private void swap(int[] nums, int i, int j) {
-26        int temp = nums[i];
-27        nums[i] = nums[j];
-28        nums[j] = temp;
-29    }
-30    private void reverse(int[] nums, int start, int end) {
-31        while (start < end) {
-32            swap(nums, start++, end--);
-33        }
-34    }
-35}
+1class Solution {
+2    public void nextPermutation(int[] nums) {
+3        int ind1 = -1;
+4        int ind2 = -1;
+5
+6        // Step 1: Find the first decreasing element from the end
+7        for (int i = nums.length - 2; i >= 0; i--) {
+8            if (nums[i] < nums[i + 1]) {
+9                ind1 = i;
+10                break;
+11            }
+12        }
+13
+14        // If no such element is found, reverse the whole array
+15        if (ind1 == -1) {
+16            reverse(nums, 0, nums.length - 1);
+17        } else {
+18            // Step 2: Find the element just greater than nums[ind1] to swap with
+19            for (int i = nums.length - 1; i > ind1; i--) {
+20                if (nums[i] > nums[ind1]) {
+21                    ind2 = i;
+22                    break;
+23                }
+24            }
+25
+26            swap(nums, ind1, ind2);
+27
+28            // Step 3: Reverse the subarray to the right of ind1
+29            reverse(nums, ind1 + 1, nums.length - 1);
+30        }
+31    }
+32
+33    private void swap(int[] nums, int i, int j) {
+34        int temp = nums[i];
+35        nums[i] = nums[j];
+36        nums[j] = temp;
+37    }
+38
+39    private void reverse(int[] nums, int start, int end) {
+40        while (start < end) {
+41            swap(nums, start++, end--); // <-- fixed post-decrement
+42        }
+43    }
+44}
+45
